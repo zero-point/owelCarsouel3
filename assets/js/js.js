80ce68720f39285d96fa0941a75ed0e2151d0474
kharazzoro <kha.1991.14.2017@gmail.com>
2017-07-02 23:10:24 +0100

    first commit

---
+++ assets/js/js.js
@@ -0,0 +1,19 @@
+// var b=document.querySelector("div.owl-next");
+// var a=document.querySelector(".owl-prev");
+// a.style.display="none";
+// b.style.display="none";
+//
+// var gribCoursela = document.querySelector('.sliderInside');
+// gribCoursela.addEventListener("mouseover",function(){
+//
+//
+//                   a.style.display="inline-block";
+//                   b.style.display="inline-block";
+//                 });
+//
+// gribCoursela.addEventListener("mouseleave",function(){
+//
+//
+//                   a.style.display="none";
+//                   b.style.display="none";
+//                 });
